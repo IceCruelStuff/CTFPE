38a0e8f05898480ece007792883cf7b45ccbab61
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-04-18 11:09:45 -0700

    Main: update

--- src/CTF/Main.php
+++ src/CTF/Main.php
@@ -1,75 +1,62 @@
 <?php
-namespace CTF;
 
+namespace CTF;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-
 use pocketmine\Player;
 use pocketmine\Server;
-
 use pocketmine\utils\Config;
-
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageByChildEntityEvent;
-
 use pocketmine\event\player\PlayerJoinEvent;
 
-
-class Main extends PluginBase implements Listener
-{
+class Main extends PluginBase implements Listener {
 
     public function onLoad(){
-       @mkdir($this->getDataFolder());
+        @mkdir($this->getDataFolder());
         $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(/*todo*/));
-		$this->config->save();
+        $this->config->save();
         $this->getServer()->getLogger()->info("[CTF]Plugin is loading!");
-     }
-}
-    
+    }
+
     public function onEnable() {
-		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new gameTask($this),20);
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new gameTask($this),20);
         $this->getServer()->getLogger()->info("[CTF]Plugin has been enabled!");
-		$this->redPlayers = []; //max amount is 5
-		$this->bluePlayers = [];
-		$this->kick = [];
-											  
-} 
+        $this->redPlayers = []; //max amount is 5
+        $this->bluePlayers = [];
+        $this->kick = [];
+    }
 
+    public function addRedPlayer(Player $p){
+        $this->redPlayers[$p->getName()] = array("red" => $p->getName());
+    }
 
-public function addRedPlayer(Player $p){
-	$this->redPlayers[$p->getName()] = array("red" => $p->getName());
-}
+    public function addBluePlayer(Player $p){
+        $this->bluePlayers[$p->getName()] = array("blue" => $p->getName());
+    }
 
-public function addBluePlayer(Player $p){
-	$this->bluePlayers[$p->getName()] = array("blue" => $p->getName());
-}
+    public function pickTeam(Player $p){
+        if(count($this->redPlayers) === 5 && count($this->bluePlayers) === 5){
+            $p->sendMessage("All teams are full! Removing you from the server! in 2 seconds!");
+            array_push($this->kick,$p);
+            $task = new kickTask($this, $this);
+            $this->getServer()->getScheduler()->scheduleDelayedTask($task, 20*2);
+            return;
+	}
 
-public function pickTeam(Player $p){
-if(count($this->redPlayers) === 5 && count($this->bluePlayers) === 5){
-	$p->sendMessage("All teams are full! Removing you from the server! in 2 seconds!");
-	array_push($this->kick,$p);
-	$task = new kickTask($this, $this);
-	$this->getServer()->getScheduler()->scheduleDelayedTask($task, 20*2);
-	return;
-} 
+        if(count($this->bluePlayers) < count($this->redPlayers)){
+	    $this->addBluePlayer($p);
+	    //tp to blue spot
+        } else {
+            $this->addRedPlayer($p);
+            //tp to red spot
+        }
+    }
 
-if(count($this->bluePlayers) < count($this->redPlayers)){
-	$this->addBluePlayer($p);
-	//tp to blue spot
-}else{
-	$this->addRedPlayer($p);
-	//tp to red spot
-}
-
-}
-
-
-public function onJoin(PlayerJoinEvent $ev){
-	$p = $ev->getPlayer();
-	$this->pickTeam($p);
-	
-}
-
+    public function onJoin(PlayerJoinEvent $ev){
+        $p = $ev->getPlayer();
+        $this->pickTeam($p);	
+    }
 }
