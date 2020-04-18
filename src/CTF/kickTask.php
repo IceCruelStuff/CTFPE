345167e6fb94073d45442be5761f23a0e4bcb6f5
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-04-18 11:12:09 -0700

    kickTask: small fix

--- src/CTF/kickTask.php
+++ src/CTF/kickTask.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace CTF;
 
 use pocketmine\scheduler\PluginTask;
@@ -6,20 +7,21 @@ use pocketmine\plugin\Plugin;
 use pocketmine\Player;
 
 class kickTask extends PluginTask{
-	public $player;
-	public function __construct(Plugin $owner, Player $player){
-		parent::__construct($owner);
-		$this->player = $player;
-		 $config = $this->getOwner()->getConfig();
-	}
-	
-	
-	public function onRun($currentTick){
-		if($this->player instanceof Player){
-			if(in_array($this->player->getName(), $this->getOwner()->kick)){
-				unset($this->getOwner()->compass[$id]);
-				$this->player->close("","You have been kicked!");
-				}
-	}
-}
+
+    public $player;
+
+    public function __construct(Plugin $owner, Player $player){
+        parent::__construct($owner);
+        $this->player = $player;
+        $config = $this->getOwner()->getConfig();
+    }
+
+    public function onRun($currentTick){
+        if($this->player instanceof Player){
+            if(in_array($this->player->getName(), $this->getOwner()->kick)){
+                unset($this->getOwner()->compass[$id]);
+                $this->player->close("","You have been kicked!");
+            }
+        }
+    }
 }
